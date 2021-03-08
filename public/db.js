09877fd4921863a73136f977c93ce42b8c3f54ed
banuakman <banu@asterya.com>
2021-03-07 20:36:59 -0500

    add saveRecord and checkDatabase functions

--- public/db.js
+++ public/db.js
@@ -19,3 +19,42 @@ request.onsuccess = function (event) {
 request.onerror = function (event) {
 	console.log("Woops! " + event.target.errorCode);
 };
+
+function saveRecord(record) {
+	// create a transaction on the pending db with readwrite access
+	const transaction = db.transaction(["pending"], "readwrite");
+	// access your pending object store
+	const store = transaction.objectStore("pending");
+	// add record to your store with add method.
+	store.add(record);
+}
+
+function checkDatabase() {
+	const transaction = db.transaction(["pending"], "readwrite");
+	const store = transaction.objectStore("pending");
+	const getAll = store.getAll();
+
+	getAll.onsuccess = function () {
+		if (getAll.result.length > 0) {
+			fetch("/api/transaction/bulk", {
+				method: "POST",
+				body: JSON.stringify(getAll.result),
+				headers: {
+					Accept: "application/json, text/plain, */*",
+					"Content-Type": "application/json",
+				},
+			})
+				.then((response) => response.json())
+				.then(() => {
+					// if successful, open a transaction on your pending db
+					const transaction = db.transaction(["pending"], "readwrite");
+					// access your pending object store
+					const store = transaction.objectStore("pending");
+					// clear all items in your store
+					store.clear();
+				});
+		}
+	};
+}
+
+window.addEventListener("online", checkDatabase);
